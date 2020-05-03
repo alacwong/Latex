1b1fb1efa51795f8a8a60237b7478ca89d993c38
alacwong <46137625+alacwong@users.noreply.github.com>
2020-05-03 14:57:54 -0400

    Added labeling for bounding polygon and added detection of multiple objects

--- src/image/vision.py
+++ src/image/vision.py
@@ -1,1 +1,32 @@
 # library for reading images using vision api
+import os
+from google.cloud import vision
+import src.image.image_process as im
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'credentials.json'
+client = vision.ImageAnnotatorClient()
+
+
+def read_image(path: str):
+    """
+    Return text annotated image
+    :param path: path to image
+    :return: api annotated image
+    """
+    content = im.read_image(path)
+    content = im.image_to_byte(content)
+    image = vision.types.Image(content=content)
+    return client.document_text_detection(image=image)
+
+
+def read_objects(path: str):
+    """
+    Find objects in image
+    :param path: path to image
+    :return list of objects in image:
+    """
+    content = im.read_image(path)
+    content = im.image_to_byte(content)
+    image = vision.types.Image(content=content)
+    return client.object_localization(
+        image=image).localized_object_annotations
