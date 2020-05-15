d76883a2532ae6ff408b8983dd9d5d85cc068bd0
alacwong <46137625+alacwong@users.noreply.github.com>
2020-05-15 18:59:53 -0400

    Wrote skeleton code for parsing 2d expression

---
+++ src/machine_learning/Parser.py
@@ -0,0 +1,26 @@
+# Library to parse 2D mathematical objects
+
+# Recursively iterate through symbols as a best first search
+# Add check if you can add any nodes, to the tree through
+# our cost function, if not, branch dies,
+# how to select next stroke .. check every stroke and evaluate
+# have a threshold for both, and check all
+# check position, and does it make sense
+# which root to check?
+# use dp on sub expressions
+# try tree struct for combining expressions
+# generate each expression first,
+
+
+def spatial_cost():
+    """
+    compute cost of node given spatial relationship
+    :return:
+    """
+
+
+def contextual_cost():
+    """
+    compute cost of node given context of equation
+    :return:
+    """
